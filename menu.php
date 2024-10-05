347d13bfc2ab1d5142fbaead09ab18759054936e
Mélanie Bethermat <melaniebethermat@gmail.com>
2024-10-05 19:54:18 +0200

    modif menu

--- menu.php
+++ menu.php
@@ -6,6 +6,19 @@
     <title>Menu</title>
 </head>
 <body>
+    <header>
+        <!-- BARRE DE MENU AVEC LES ONGLETS -->
+        <nav class="barreNav">
+            <li><a href="/Presentation/index.html#ancreAccueil">Accueil</a></li>
+            <li><a href="/">Menu</a></li>
+            <li><a href="/page3SuggestionChef/page3Suggestion.html#ancreSuggestion">Suggestion du Chef</a></li>
+            <li><a href="/page4Horaires/page4Horaires.html#ancreHoraires">Horaires</a></li>
+            <li><a href="/pageContactHtml/page5Contact.html#pageContact">Nous contacter</a></li>
+            <li><a href="/page6LivreDor/livreDOr.html#ancreLivreDOr">Livre d'or</a></li>
+        </nav>
+    </header>
+
+
     <!-- Votre contenu HTML -->
     <?php
         include 'db.php'; // assume this file has your database connection details
@@ -32,5 +45,26 @@ if ($result->num_rows > 0) {
 mysqli_close($conn);
 ?>
     
+
+    <footer>
+            <div class="containerAdresse">
+                <i class="fa-solid fa-location-dot"></i>
+                <p id="nomLocation">Location</p>
+                <p id="adresseParagraphe">Restaurant Stromboli, 34 Av Charles Boutet <br>
+                    08000 Charleville-Mézières</p>
+            </div>
+
+
+
+            <div class="iconContainer">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+
+            <div>
+            <p id="copyright">Copyright 2024</p>
+            </div>
+        </footer>
 </body>
 </html>
